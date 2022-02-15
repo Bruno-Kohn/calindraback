20c970e6963ed58c753f10493a6304698a7bd702
Bruno-Kohn <brunokohn1993@gmail.com>
2022-02-15 19:31:30 -0300

    feat: api done

--- src/controllers/check.js
+++ src/controllers/check.js
@@ -1,5 +1,48 @@
-/*
-Av.%20Rio%20Branco,%201%20Centro,%20Rio%20de%20Janeiro%20RJ,%2020090003;%20Praça%20Mal.%20Âncora,%20122%20Centro,%20Rio%20de%20Janeiro%20RJ,%2020021200;%20Rua%2019%20de%20Fevereiro,%2034%20Botafogo,%20Rio%20de%20Janeiro%20RJ,%2022280030 1
-Av.%20Rio%20Branco,%201%20Centro,%20Rio%20de%20Janeiro%20RJ,%2020090003;%20Praça%20Mal.%20Âncora,%20122%20Centro,%20Rio%20de%20Janeiro%20RJ,%2020021200;%20Rua%2019%20de%20Fevereiro,%2034%20Botafogo,%20Rio%20de%20Janeiro%20RJ,%2022280030 2
-https://maps.googleapis.com/maps/api/geocode/json?address=Av.%20Rio%20Branco,%201%20Centro,%20Rio%20de%20Janeiro%20RJ,%2020090003;%20Praça%20Mal.%20Âncora,%20122%20Centro,%20Rio%20de%20Janeiro%20RJ,%2020021200;%20Rua%2019%20de%20Fevereiro,%2034%20Botafogo,%20Rio%20de%20Janeiro%20RJ,%2022280030&key=AIzaSyDPcJoqztWHCikJ5GKkEoC3gpZQ8StoAGs 3
-*/
+const result = {
+  distances: [
+    {
+      address1:
+        'Av. Rio Branco, 1 - Centro, Rio de Janeiro - RJ, 20090-003, Brazil',
+      address2:
+        'R. Gen. Belegarde, 232 - Engenho Novo, Rio de Janeiro - RJ, 20710-003, Brazil',
+      distance: 0.0928255440457027
+    },
+    {
+      address1:
+        'Av. Rio Branco, 1 - Centro, Rio de Janeiro - RJ, 20090-003, Brazil',
+      address2:
+        'R. Dezenove de Fevereiro, 34 - Botafogo, Rio de Janeiro - RJ, 22280-030, Brazil',
+      distance: 0.05389845314153423
+    },
+    {
+      address1:
+        'Av. Rio Branco, 1 - Centro, Rio de Janeiro - RJ, 20090-003, Brazil',
+      address2:
+        'R. José Higino, 253 - Tijuca, Rio de Janeiro - RJ, 20520-201, Brazil',
+      distance: 0.06639186653936281
+    },
+
+    {
+      address1:
+        'R. Gen. Belegarde, 232 - Engenho Novo, Rio de Janeiro - RJ, 20710-003, Brazil',
+      address2:
+        'R. Dezenove de Fevereiro, 34 - Botafogo, Rio de Janeiro - RJ, 22280-030, Brazil',
+      distance: 0.09395813308431662
+    },
+    {
+      address1:
+        'R. Gen. Belegarde, 232 - Engenho Novo, Rio de Janeiro - RJ, 20710-003, Brazil',
+      address2:
+        'R. José Higino, 253 - Tijuca, Rio de Janeiro - RJ, 20520-201, Brazil',
+      distance: 0.038981072907244445
+    },
+    {
+      address1:
+        'R. Dezenove de Fevereiro, 34 - Botafogo, Rio de Janeiro - RJ, 22280-030, Brazil',
+      address2:
+        'R. José Higino, 253 - Tijuca, Rio de Janeiro - RJ, 20520-201, Brazil',
+      distance: 0.055150154395887434
+    }
+  ],
+  nearAdressess: 'sfkfvbsfhk'
+};
--- src/controllers/getAddress.js
+++ src/controllers/getAddress.js
@@ -10,7 +10,47 @@ async function getAddress(req, res) {
 
     const reqExternalAPI = axios.post(URL);
 
-    reqExternalAPI.then((resp) => res.status(200).send(resp.data));
+    reqExternalAPI.then((resp) => {
+      const geo = resp.data.results.map((i) => ({
+        address: i.formatted_address,
+        lat: i.geometry.location.lat,
+        lng: i.geometry.location.lng
+      }));
+
+      const distanceConfig = [];
+
+      for (let i = 0; i < geo.length; i++) {
+        for (let j = i + 1; j < geo.length; j++) {
+          distanceConfig.push({
+            address1: geo[i].address,
+            address2: geo[j].address,
+            distance: Math.sqrt(
+              (geo[j].lat - geo[i].lat) ** 2 + (geo[j].lng - geo[i].lng) ** 2
+            )
+          });
+        }
+      }
+
+      const nearAddresses = [];
+
+      for (let i = 0; i < geo.length; i++) {
+        nearAddresses.push({
+          referenceAddress: geo[i].address,
+          addressesByDistance: [
+            distanceConfig
+              .filter(
+                (j) =>
+                  j.address1 === geo[i].address || j.address2 === geo[i].address
+              )
+              .sort(function (a, b) {
+                return a.distance - b.distance;
+              })
+          ]
+        });
+      }
+
+      res.status(200).send({ distance: distanceConfig, nearAddresses });
+    });
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
