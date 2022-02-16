36f42eddfbfe322e10c1a469ee0aab2b8ef114c7
Bruno-Kohn <brunokohn1993@gmail.com>
2022-02-16 09:57:18 -0300

    feat: swagger implemented

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import { getAddress } from './controllers/getAddress.js';
+import swaggerUi from 'swagger-ui-express';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const swaggerDocs = require('./swagger.json');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs)); //http://localhost:4000/api-docs/
 
 app.post('/address', getAddress);
 
